c26f80edf7a70a4751a19c464bfa6854194eaef6
Nikhil <nikhil@Nikhil-ThinkPad.(none)>
2013-02-12 16:15:14 +0800

    (ugly) map interaction working

--- app/scripts/drawMap.js
+++ app/scripts/drawMap.js
@@ -1,4 +1,4 @@
-define(['leaflet', 'jquery'], function() {
+define(['leaflet', 'jquery', 'topojson'], function() {
 	require(['asia'], function(asia) {
 
 	// draw map centered on asia
@@ -7,15 +7,30 @@ define(['leaflet', 'jquery'], function() {
 		zoom: 4,
 		scrollWheelZoom: false,
 		worldCopyJump: true,
-		inertiaMaxSpeed: 1000
+		inertiaMaxSpeed: 600
 	});
 
 	L.tileLayer('http://d.tiles.mapbox.com/v3/nikhils.map-rrpbmuxw/{z}/{x}/{y}.png', {
     attribution: '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
 }).addTo(map);
 
-// pan to map when clicking on country name,
-// center on country clicked
+// map info UI
+var info = L.control();
+
+	info.onAdd = function(map) {
+		this._div = L.DomUtil.create('div', 'infobox');
+		return this._div;
+		};
+
+	info.update = function (place) {
+		var lookup = locale[place];
+    this._div.innerHTML = '<h4>Country details</h4>' + place + " " + lookup.gdppc + " " + lookup.totalPop;
+};
+
+info.addTo(map);
+
+// scroll to map when clicking on country name, center on country clicked
+// give info for that country
 	var locale = asia.locales;
 
 	var panMap = function(place) {
@@ -33,8 +48,63 @@ define(['leaflet', 'jquery'], function() {
     $('body').animate({ scrollTop: pos.top - center });
     e.preventDefault();
 
-    panMap($(this).html())
-
+    panMap($(this).html());
+    info.update($(this).html());
 		});
+
+// do fun stuff when we mouse over a country
+	function onEachFeature(feature, layer) {
+    layer.on({
+        mouseover: mapHover,
+        mouseout: styleReset
+			})
+  };
+
+  function mapHover(e) {
+   	layer = e.target;
+   	layer.setStyle({
+        weight: 2,
+        color: '#666',
+        fillOpacity: 0.3
+    	});
+   	info.update(layer.feature.properties.name)
+   };
+
+   function styleReset(e) {
+    countriesLayer.resetStyle(e.target);
+   }
+
+   // draw layer for asian countries
+	var countriesLayer = L.geoJson(null, {
+        style: {
+            color: '#666',
+            weight: 0,
+            fillOpacity: 0.1,
+        },
+        onEachFeature: onEachFeature
+     });
+
+	$.getJSON('../data/asia-topo.json', function (data) {
+    var asia_geojson = topojson.object(data, data.objects.asia);
+   
+    var featureCollection = {
+      "type": "FeatureCollection",
+      "features": []
+    };
+
+   // retain necessary properties of topojson file
+	   for (var i = 0; i < asia_geojson.geometries.length; i++) {
+	      featureCollection.features.push({
+	        "type":"Feature",
+	        "geometry": asia_geojson.geometries[i],
+	        "properties": asia_geojson.geometries[i].properties
+	      	});
+	      }	
+	     countriesLayer.addData(featureCollection)
+	     console.log(featureCollection)
+		});
+
+	map.addLayer(countriesLayer);
+
 	});
 });
--- app/scripts/main.js
+++ app/scripts/main.js
@@ -4,24 +4,23 @@ require.config({
     jquery: 'vendor/jquery.min',
     d3: '/components/d3/d3.min',
     leaflet: '/components/leaflet/dist/leaflet',
-    waypoints: 'vendor/waypoints.min'
+    waypoints: 'vendor/waypoints.min',
+    topojson: 'vendor/topojson'
     // mustache: '/components/mustache/mustache'
   },
 
   shim: {
-    'waypoints': ['jquery'],
-    'scroll': ['jquery']
+    'waypoints': ['jquery']
   }
 
 });
 
-var scripts = ['drawMap', 'app', 'waypoint', 'scroll'];
+var scripts = ['drawMap', 'app', 'waypoint'];
 
-require(scripts, function(drawMap, app, waypoint, scroll) {
+require(scripts, function(drawMap, app, waypoint) {
   // use app here
   drawMap;
   app;
   waypoint;
-  scroll;
 
 });
