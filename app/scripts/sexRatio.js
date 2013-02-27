2eedf839fe7648d0bc50ea839a4941c6e90e275f
Nikhil <nikhil@Nikhil-ThinkPad.(none)>
2013-02-27 12:23:47 +0800

    hinting and linting

--- app/scripts/sexRatio.js
+++ app/scripts/sexRatio.js
@@ -1,152 +1,138 @@
-require(['d3', 'jquery'], function() {
+require(['d3'], function () {
 
-var 	margin = {t:20, r:40, b:40, l:40 },
-		w = 960 - margin.l - margin.r,
-		h = 580 - margin.t - margin.b,
-		x = d3.scale.ordinal().rangeRoundBands([0, w], .07),
-		y = d3.scale.linear().rangeRound([h, 0])
-		color = d3.scale.category10(),
-		forceW = w-250;
+    'use strict';
 
-var	data,
-		girlPath = 'M21.022,16.349c-0.611-1.104-1.359-1.998-2.109-2.623c-0.875,0.641-1.941,1.031-3.104,1.031c-1.164,0-2.231-0.391-3.105-1.031c-0.75,0.625-1.498,1.519-2.111,2.623c-1.422,2.563-1.579,5.192-0.351,5.874c0.55,0.307,1.127,0.078,1.723-0.496c-0.105,0.582-0.167,1.213-0.167,1.873c0,2.932,1.139,5.307,2.543,5.307c0.846,0,1.265-0.865,1.466-2.189c0.201,1.324,0.62,2.189,1.464,2.189c1.406,0,2.545-2.375,2.545-5.307c0-0.66-0.061-1.291-0.168-1.873c0.598,0.574,1.174,0.803,1.725,0.496C22.603,21.541,22.444,18.912,21.022,16.349zM15.808,13.757c2.363,0,4.279-1.916,4.279-4.279s-1.916-4.279-4.279-4.279c-2.363,0-4.28,1.916-4.28,4.279S13.445,13.757,15.808,13.757zM18.731,4.974c1.235,0.455,0.492-0.725,0.492-1.531s0.762-1.792-0.492-1.391c-1.316,0.422-2.383,0.654-2.383,1.461S17.415,4.489,18.731,4.974zM15.816,4.4c0.782,0,0.345-0.396,0.345-0.884c0-0.488,0.438-0.883-0.345-0.883s-0.374,0.396-0.374,0.883C15.442,4.005,15.034,4.4,15.816,4.4zM12.884,4.974c1.316-0.484,2.383-0.654,2.383-1.461S14.2,2.474,12.884,2.052c-1.254-0.402-0.492,0.584-0.492,1.391S11.648,5.428,12.884,4.974z',
-		boyPath = 'M21.021,16.349c-0.611-1.104-1.359-1.998-2.109-2.623c-0.875,0.641-1.941,1.031-3.103,1.031c-1.164,0-2.231-0.391-3.105-1.031c-0.75,0.625-1.498,1.519-2.111,2.623c-1.422,2.563-1.578,5.192-0.35,5.874c0.55,0.307,1.127,0.078,1.723-0.496c-0.105,0.582-0.166,1.213-0.166,1.873c0,2.932,1.139,5.307,2.543,5.307c0.846,0,1.265-0.865,1.466-2.189c0.201,1.324,0.62,2.189,1.463,2.189c1.406,0,2.545-2.375,2.545-5.307c0-0.66-0.061-1.291-0.168-1.873c0.598,0.574,1.174,0.803,1.725,0.496C22.602,21.541,22.443,18.912,21.021,16.349zM15.808,13.757c2.362,0,4.278-1.916,4.278-4.279s-1.916-4.279-4.278-4.279c-2.363,0-4.28,1.916-4.28,4.279S13.445,13.757,15.808,13.757z';
+    var
+    margin = { t : 20, r : 40, b : 40, l : 40 },
+    w = 960 - margin.l - margin.r,
+    h = 580 - margin.t - margin.b,
+    forceW = w - 300;
 
-centers = {
-  "boy": {x: forceW / 3, y: h / 2},
-  "girl": {x: forceW / 2, y: h / 2},
-  "missing": {x: 2 * forceW / 3, y: h / 2}
-};
+    var
+    data,
+    girlPath = 'M21.022,16.349c-0.611-1.104-1.359-1.998-2.109-2.623c-0.875,0.641-1.941,1.031-3.104,1.031c-1.164,0-2.231-0.391-3.105-1.031c-0.75,0.625-1.498,1.519-2.111,2.623c-1.422,2.563-1.579,5.192-0.351,5.874c0.55,0.307,1.127,0.078,1.723-0.496c-0.105,0.582-0.167,1.213-0.167,1.873c0,2.932,1.139,5.307,2.543,5.307c0.846,0,1.265-0.865,1.466-2.189c0.201,1.324,0.62,2.189,1.464,2.189c1.406,0,2.545-2.375,2.545-5.307c0-0.66-0.061-1.291-0.168-1.873c0.598,0.574,1.174,0.803,1.725,0.496C22.603,21.541,22.444,18.912,21.022,16.349zM15.808,13.757c2.363,0,4.279-1.916,4.279-4.279s-1.916-4.279-4.279-4.279c-2.363,0-4.28,1.916-4.28,4.279S13.445,13.757,15.808,13.757zM18.731,4.974c1.235,0.455,0.492-0.725,0.492-1.531s0.762-1.792-0.492-1.391c-1.316,0.422-2.383,0.654-2.383,1.461S17.415,4.489,18.731,4.974zM15.816,4.4c0.782,0,0.345-0.396,0.345-0.884c0-0.488,0.438-0.883-0.345-0.883s-0.374,0.396-0.374,0.883C15.442,4.005,15.034,4.4,15.816,4.4zM12.884,4.974c1.316-0.484,2.383-0.654,2.383-1.461S14.2,2.474,12.884,2.052c-1.254-0.402-0.492,0.584-0.492,1.391S11.648,5.428,12.884,4.974z',
+    boyPath = 'M21.021,16.349c-0.611-1.104-1.359-1.998-2.109-2.623c-0.875,0.641-1.941,1.031-3.103,1.031c-1.164,0-2.231-0.391-3.105-1.031c-0.75,0.625-1.498,1.519-2.111,2.623c-1.422,2.563-1.578,5.192-0.35,5.874c0.55,0.307,1.127,0.078,1.723-0.496c-0.105,0.582-0.166,1.213-0.166,1.873c0,2.932,1.139,5.307,2.543,5.307c0.846,0,1.265-0.865,1.466-2.189c0.201,1.324,0.62,2.189,1.463,2.189c1.406,0,2.545-2.375,2.545-5.307c0-0.66-0.061-1.291-0.168-1.873c0.598,0.574,1.174,0.803,1.725,0.496C22.602,21.541,22.443,18.912,21.021,16.349zM15.808,13.757c2.362,0,4.278-1.916,4.278-4.279s-1.916-4.279-4.278-4.279c-2.363,0-4.28,1.916-4.28,4.279S13.445,13.757,15.808,13.757z';
 
-var sexRatio = d3.select('#sexRatio').append('svg')
-		.attr("width",w + margin.l + margin.r)
-		.attr("height",h + margin.t + margin.b)
-	.append("g")
-		.attr({
-			transform: "translate(330," + margin.t +")",
-			class: "sexGroup"
-		});
+    var sexRatio = d3.select('#sexRatio').append('svg')
+        .attr('width', w + margin.l + margin.r)
+        .attr('height', h + margin.t + margin.b)
+    .append('g')
+        .attr({
+            transform: 'translate(330,' + margin.t + ')',
+            class: 'sexGroup'
+        });
 
-var sexControls = d3.select('#sexRatio').append('div')
-	.attr('width', 200)
-	.attr("class","sexControls")
-	.html('<h2>View sex ratio data for:</h2>')
+    var sexControls = d3.select('#sexRatio').append('div')
+        .attr('width', 200)
+        .attr('class', 'sexControls')
+        .html('<h2>View sex ratio data for:</h2>');
 
-var sexLegend = sexControls.append('div').attr('class', 'sexLegend')
+    sexControls.append('div')
+        .attr('class', 'sexLegend');
 
-var force = d3.layout.force().size([forceW, h])
-    .charge(-70).alpha(0);
+    var force = d3.layout.force()
+        .size([forceW, h])
+        .charge(-40).alpha(0);
 
-d3.csv('../data/sex-ratio.csv', function(csv) {
-	data = csv;
+    d3.csv('../data/sex-ratio.csv', function (csv) {
+        data = csv;
 
-	sexControls.insert('div', '.sexLegend').attr('class', 'sexCountries')
-	.append('ul').attr("class","countries")
-	.selectAll('li')
-		.data(csv)
-	.enter().append('li')
-		.attr("class",'countryLink')
-		.append('a')
-		.style('cursor', 'pointer')
-		.text(function(d) { return d.country; })
+        sexControls.insert('div', '.sexLegend').attr('class', 'sexCountries')
+            .append('ul').attr('class', 'countries')
+            .selectAll('li')
+            .data(csv)
+        .enter().append('li')
+            .attr('class', 'countryLink')
+            .append('a')
+            .style('cursor', 'pointer')
+            .text(function (d) { return d.country; });
 
-	data.map(function(d) {
-		d.node = {name: d.country, boys: 105, 
-		girls: 100 - d["missing-girls"], missing: +d["missing-girls"] };
-	});
+        data.map(function (d) {
+            d.node = {name: d.country, boys: 105,
+            girls: 100 - d['missing-girls'], missing: +d['missing-girls'] };
+        });
 
-	sexControls.selectAll('.countryLink').on('click', update)
+        sexControls.selectAll('.countryLink')
+            .on('click', function (d) { return drawNodes(d.country); });
 
-});
+    });
 
-function drawNodes(country) {
+    function drawNodes(country) {
 
-// use only data from selected country
-	var filtered = data.filter(function(d, i){
-		return d.country === country;
-	});
+        // use only data from selected country
+        var filtered = data.filter(function (d) {
+            return d.country === country;
+        });
 
-	console.log(filtered)
+        d3.select('.sexLegend').html(function () {
+            var a = filtered[0];
+            return '<p>In 2010 <strong>' + a.country + '</strong> registered ' + commas(a.births) + ' births. At the natural rate of 105 boys for every 100 girls, ' + commas(a.natural) + ' girls should have been born that year. But at its ratio of ' + a['sex-ratio'] + ' girls for every 1,000 boys, only ' + commas(a.est) + ' were born. That means <strong>' + commas(a.diff) + ' girls are "missing"</strong> for 2010 alone. And for every 105 boys, ' + a.node.missing + ' more girls should have been born.</p><h3>105<svg width="25px" height="20px"><path transform=scale(1.2) class="boyPath" d="' + boyPath + '" /></svg></h3><h3>' + a.node.girls + '<svg width="25px" height="20px"><path transform=scale(1.2) class="girlPath" d="' + girlPath + '" /></svg></h3><h3>' + a.node.missing + '<svg width="25px" height="20px"><path transform=scale(1.2) class="missingPath" d="' + girlPath + '" /></svg></h3><p><em>Data are from the World Bank and UN</em></p>';
+        });
 
-	$('.sexLegend').html(function() {
-		var a = filtered[0];
-		return '<p>In 2010 <strong>' + a.country + '</strong> registered ' + commas(a.births) + ' births. At the natural rate of 105 boys for every 100 girls, ' + commas(a.natural) + ' girls should have been born that year. But at its ratio of '+ a['sex-ratio'] + ' girls for every 1,000 boys, only ' + commas(a.est) + ' were born. That means ' + commas(a.diff) + ' girls are "missing" for 2010 alone. And for every 105 boys, ' +a.node.missing+ ' more girls should have been born.</p><h3>105<svg width="25px" height="20px"><path transform=scale(1.2) class="boyPath" d="'+boyPath+'" /></svg></h3><h3>' + a.node.girls + '<svg width="25px" height="20px"><path transform=scale(1.2) class="girlPath" d="'+girlPath+'" /></svg></h3><h3>' + a.node.missing + '<svg width="25px" height="20px"><path transform=scale(1.2) class="missingPath" d="'+girlPath+'" /></svg></h3><p><em>Data are from the World Bank and UN</em></p>'
-		});
+        // create array with entries for each boy, girl or missing girl
+        var nodeArray = [];
+        for (var i = 0; i < filtered[0].node.boys; i++) {
+            nodeArray.push(['boy']);
+        }
+        for (i = 0; i < filtered[0].node.girls; i++) {
+            nodeArray.push(['girl']);
+        }
+        for (i = 0; i < filtered[0].node.missing; i++) {
+            nodeArray.push(['missing']);
+        }
 
-// create array with entries for each boy, girl or missing girl
-	var nodeArray = [];
-		for (var i=0; i<filtered[0].node.boys; i++) {
-			nodeArray.push(["boy"])
-		};
-		for (var i=0; i<filtered[0].node.girls; i++) {
-			nodeArray.push(["girl"])
-		};
-		for (var i=0; i<filtered[0].node.missing; i++) {
-			nodeArray.push(["missing"])
-		};
+        var node = sexRatio.selectAll('.node')
+            .data(nodeArray);
 
-force
-    .nodes(nodeArray)
-    .on("tick", tick)
-    .start();
+        node.exit().remove();
 
-	var node = sexRatio.selectAll(".node")
- 		.data(nodeArray)
+        var nodeEnter = node.enter().append('g')
+            .attr('class', 'node');
 
- 	node.exit().remove()
+        var icons = node.selectAll('path')
+            .data(function (d) { return d; })
+        .enter().append('path')
+            .attr('class', 'iconPath')
+            .attr('d', girlPath);
 
-	nodeEnter = node.enter().append("g")
-		.attr("class", "node")
-	
-	var icons = node.selectAll('path')
-		.data(function(d) { return d; })
-	.enter().append('path')
-		.attr("class",'iconPath')
+        d3.selectAll('.iconPath').transition()
+            .each(function (d) {
+            var path = d3.select(this);
 
-	d3.selectAll('.iconPath').transition()
-		.each(function(d) {
-		 	path = d3.select(this)
-		 	if (d === 'girl') {
-				path
-		 		.attr('d', girlPath)
-		 		.style('stroke', 'none')
-		 		.style('fill', 'orange')
-		 	} else if (d === 'boy') {
-		 		path
-		 		.attr('d', boyPath)
-		 		.style('stroke', 'none')
-		 		.style('fill', '#B83834')
-		 	} else { 
-		 		path
-		 		.attr('d', girlPath)
-		 		.style('fill-opacity', '0')
-		 		.style('stroke', '#E98125')
-		 		.style('stroke-width', '1')
-		 	}
-		});
+            if (d === 'girl') {
+                path.attr('d', girlPath)
+                    .style('stroke', 'none')
+                    .style('fill', 'orange');
+            } else if (d === 'boy') {
+                path.attr('d', boyPath)
+                    .style('stroke', 'none')
+                    .style('fill', '#B83834');
+            } else {
+                path.attr('d', girlPath)
+                    .style('fill-opacity', '0')
+                    .style('stroke', '#E98125')
+                    .style('stroke-width', '1');
+            }
+        });
 
-	// icons.attr("transform","scale(1.3)")
+        icons.attr('transform', 'scale(1.2)');
 
-	nodeEnter.call(force.drag);
+        nodeEnter.call(force.drag);
 
-	function tick(e) {
-		var k = .2 * e.alpha;
-	  	nodeArray.forEach(function(o, i) {
-	    o.y += (centers[o].y - o.y) * k;
-	    o.x += (centers[o].x - o.x) * k;
-	  	});
+        function tick() {
+            node.attr('transform', function (d) {
+                return 'translate(' + d.x + ',' + d.y + ')';
+            });
+        }
 
-		node.attr("transform", function(d) { return "translate(" + d.x + "," + d.y + ")"; });
-  	};
-};
+        force
+            .nodes(nodeArray)
+            .on('tick', tick)
+            .start();
+    }
 
-function update() {
-	selected = d3.select(this).datum().country
-	drawNodes(selected)
-}
-
-function commas(x) {
-	    return x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
-	}
+    function commas(x) {
+        return x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ',');
+    }
 
 });
--- app/scripts/surveys.js
+++ app/scripts/surveys.js
@@ -1,199 +1,202 @@
-require(['d3'], function() {
+require(['d3'], function () {
+    'use strict';
 
-var margin = {t:20, r:110, b:40, l:40 },
-		w = 960 - margin.l - margin.r,
-		h = 520 - margin.t - margin.b,
-		x = d3.scale.ordinal().rangeRoundBands([0, w], .07),
-		y = d3.scale.linear().rangeRound([h, 0]),
-		color = d3.scale.ordinal(),
-    stack = d3.layout.stack();
-    formatPercent = formatPercent = d3.format(".0%");
+    var
+    margin = { t : 20, r : 110, b : 40, l : 40 },
+    w = 960 - margin.l - margin.r,
+    h = 520 - margin.t - margin.b,
+    x = d3.scale.ordinal().rangeRoundBands([0, w], 0.07),
+    y = d3.scale.linear().rangeRound([h, 0]),
+    color = d3.scale.ordinal(),
+    stack = d3.layout.stack(),
+    formatPercent = d3.format('.0%');
 
-var colorBlends = {
-	    	"blue": ["#ADCDE1","#96BFD9","#6baed6","#3182bd","#08519c"],
-	    	"red": ["#FDAD9B","#FC9078","#fb6a4a","#de2d26","#a50f15"],
-	    }
+    var data;
 
-var questions = {
-  'women-politics': "Which one of the following statements comes closest to your opinion about men and women as political leaders?...Men generally make better political leaders than women, or Women generally make better political leaders than men, or In general, women and men make equally good political leaders?",
-  'jobs': "Please tell me whether you completely agree, mostly agree, mostly disagree, or completely disagree with the following statements...When jobs are scarce, men should have more right to a job than women.",
-  'outside-home': "Please tell me whether you completely agree, mostly agree, mostly disagree, or completely disagree with the following statements...Women should be able to work outside the home."
-  };
+    var colorBlends = {
+        'blue': ['#ADCDE1', '#96BFD9', '#6baed6', '#3182bd', '#08519c'],
+        'red': ['#FDAD9B', '#FC9078', '#fb6a4a', '#de2d26', '#a50f15']
+    };
 
-var surveyData;
+    var questions = {
+        'women-politics': 'Which one of the following statements comes closest to your opinion about men and women as political leaders?...Men generally make better political leaders than women, or Women generally make better political leaders than men, or In general, women and men make equally good political leaders?',
+        'jobs': 'Please tell me whether you completely agree, mostly agree, mostly disagree, or completely disagree with the following statements...When jobs are scarce, men should have more right to a job than women.',
+        'outside-home': 'Please tell me whether you completely agree, mostly agree, mostly disagree, or completely disagree with the following statements...Women should be able to work outside the home.'
+    };
 
-var surveyChart = d3.select('#surveys').append('svg')
-	.attr("width",w + margin.l + margin.r)
-	.attr("height",h + margin.t + margin.b)
-.append("g")
-		.attr({
-			transform: "translate("+ margin.l + "," + margin.t + ")",
-			class: "surveyGroup"
-		});
+    var surveyData;
 
-var xAxis = d3.svg.axis()
-    .scale(x)
-    .tickSize(0)
-    .orient("bottom");
+    var surveyChart = d3.select('#surveys').append('svg')
+       .attr('width', w + margin.l + margin.r)
+       .attr('height', h + margin.t + margin.b)
+    .append('g')
+        .attr({
+            transform: 'translate(' + margin.l + ',' + margin.t + ')',
+            class: 'surveyGroup'
+        });
 
-var yAxis = d3.svg.axis()
-    .scale(y)
-    .orient("left")
-    .tickFormat(d3.format(".0%"));
+    var xAxis = d3.svg.axis()
+        .scale(x)
+        .tickSize(0)
+        .orient('bottom');
 
-surveyChart.append("g")
-      .attr("class", "y axis")
-      .call(yAxis);
+    var yAxis = d3.svg.axis()
+        .scale(y)
+        .orient('left')
+        .tickFormat(d3.format('.0%'));
 
-d3.json('../data/asia-surveys-master.json', function(error, json) {
-	surveyData = json;
+    surveyChart.append('g')
+          .attr('class', 'y axis')
+          .call(yAxis);
 
-  d3.select('#questText').text(questions['jobs'])
+    d3.json('../data/asia-surveys-master.json', function (error, json) {
+        surveyData = json;
 
-  x.domain(json['jobs'].map(function(d) { return d.Country }));
+        d3.select('#questText').text(questions.jobs);
 
-  surveyChart.append("g")
-    .attr("class", "x axis")
-    .attr("transform", "translate(0," + h + ")")
-    .call(xAxis);
+        x.domain(json.jobs.map(function (d) { return d.Country; }));
 
-  d3.selectAll('#surveys .x.axis text')
-    .attr("transform", "translate(" + x.rangeBand()/2 + ",15) rotate(45)")
+        surveyChart.append('g')
+            .attr('class', 'x axis')
+            .attr('transform', 'translate(0,' + h + ')')
+            .call(xAxis);
 
-  setData('jobs')
-});
+        d3.selectAll('#surveys .x.axis text')
+            .attr('transform', 'translate(' + x.rangeBand() / 2 + ',15) rotate(45)');
 
-function setData(dataset) {
-  
-  data = surveyData[dataset];
-
-	responseNames = d3.keys(data[0]).filter(function(key) { 
-    return key !== "Country" && key !== "region" && key !== "responses"})
-
-var stackData =
-    stack(responseNames.map(function(name) {
-      return data.map(function(d, i) {
-        return {name: name, region: d.region, x: d.Country, y: d[name] / 100 };
-      })
-  }));
-
-yMax = d3.max(stackData, function(layer) { return d3.max(layer, function(d) { return d.y0 + d.y; }); });
-y.domain([0, yMax])
-drawData(stackData)
-};
-
-function drawData (filtered) {
-
- d3.transition().duration(750).each(function() {
-
-	var bars = surveyChart.selectAll(".surveyRectGroup")
-    .data(filtered)
-
-  bars.exit().transition()
-    .style('opacity', '1e-6').remove()
-
-  bars.enter().append("g")
-    .attr('class', 'surveyRectGroup')
-
-  var barRect = bars.selectAll("rect")
-    .data(function(d) { return d; })
-  .enter().append('rect')
-    .attr("width", x.rangeBand())
-    .attr('x', function(d) { return x(d.x); })
-    .attr("y", function(d) { return y(d.y0 + d.y); })
-    .attr("height", 0);
-
-  var barText = bars.selectAll('.barText')
-    .data(function(d) { return d; })
-  .enter().append('text')
-    .attr("class", 'barText')
-    .style('text-anchor', 'middle')
-    .style('visibility', 'hidden')
-    .text(function(d) { return formatPercent(d.y); });
-
-  var legend = bars.selectAll('.legend')
-    .data(function(d) { return d.filter(function(c) { return c.x === 'USA'; }); })
-  
-  legend.enter().append('text')
-    .attr('class', 'legend')
-    .attr("transform", function(d) { return "translate(" + (x(d.x) + x.rangeBand()) + "," + (y(d.y0 + d.y) + (y(d.y0) - y(d.y0 + d.y))/2) + ")";} )
-    .attr("x", 5)
-    .attr("dy", ".35em")
-    .text(function(d) { return d.name; });
-
-// transitions
-  bars.selectAll('rect').transition()
-    .attr("y", function(d) { return y(d.y0 + d.y); })
-    .attr("height", function(d) { return y(d.y0) - y(d.y0 + d.y); })
-    .style("fill", function(d) { d.region === "Asia" ? 
-                      color.range(colorBlends["red"]) 
-                      : color.range(colorBlends["blue"]);
-                        return color(d.name)})
-    .each("end", function() {
-      d3.select(this).attr("class", function(d) { return cleanClass(d.name); });
+        setData('jobs');
     });
 
-  bars.selectAll('.barText').transition()
-    .attr("y", function(d) { return y(d.y0 + d.y) + (y(d.y0) - y(d.y0 + d.y))/2; })
-    .attr("x", function(d) { return x(d.x) + x.rangeBand() / 2; })
-    .attr("dy", function(d) { return (d.y) < .025 ? '.5em' : '.35em' })
-    .style('opacity', function(d) { return (d.y) === 0 ? 1e-6 : 1 })
-    .text(function(d) { return formatPercent(d.y); })
-    .each("end", function() {
-      d3.select(this).attr("class", function(d) { return cleanClass(d.name) + " barText" ; });
-    });
+    function setData(dataset) {
 
-  bars.selectAll('.legend').transition()
-    .attr("transform", function(d) { return "translate(" + (x(d.x) + x.rangeBand()) + "," + (y(d.y0 + d.y) + (y(d.y0) - y(d.y0 + d.y))/2) + ")";} )
-    .text(function(d) { return d.name; });
+        data = surveyData[dataset];
 
-  var alpha = 1.5;
-  var spacing = 12;
-  function relax(thing) {
-    var again = false;
-    thing.forEach(function(a,i) {
-      thing.slice(i+1).forEach(function(b) {
-        var dy = a.y - b.y;
-        if (Math.abs(dy) < spacing) {
-          again = true;
-          var sign = dy > 0 ? 1 : -1;
-          a.y += sign*alpha;
-          b.y -= sign*alpha;
-        }
-      });
-    });
-  };
+        var responseNames = d3.keys(data[0]).filter(function (key) {
+            return key !== 'Country' && key !== 'region' && key !== 'responses';
+        });
 
-relax(d3.selectAll('.label'));
+        var stackData =
+            stack(responseNames.map(function (name) {
+                return data.map(function (d, i) {
+                    return {name: name, region: d.region, x: d.Country, y: d[name] / 100 };
+                });
+            }));
 
-d3.selectAll(".surveyRectGroup rect").on("mouseover", mouseOn);
-d3.selectAll(".surveyRectGroup rect").on("mouseout", mouseOff);
-d3.selectAll(".barText").on("mouseover", mouseOn);
-d3.selectAll(".barText").on("mouseout", mouseOff);
- });
-};
+        var yMax = d3.max(stackData, function (layer) {
+            return d3.max(layer, function (d) {
+                return d.y0 + d.y;
+            });
+        });
 
-function cleanClass (name) {
-  return name.replace(/\s/g, '').replace('/', '');
-}
+        y.domain([0, yMax]);
 
-function mouseOn() {
-  var thisClass = d3.select(this).attr('class')
-  d3.selectAll("text." + thisClass).style('visibility', 'visible')
-  d3.select(this).style('visibility', 'visible')
-  };
-  
-function mouseOff() {
-  var thisClass = d3.select(this).attr('class')
-  d3.selectAll("text." + thisClass).style('visibility', 'hidden')
-  };
+        drawData(stackData);
+    }
 
-d3.selectAll('.surveyControl').on("click", function() {
-    var updateVal = d3.select(this).property('id');
-    setData(updateVal);
-    d3.select('#questText').text(questions[updateVal]);
+    function drawData(filtered) {
 
-    d3.selectAll('.surveyControl').style('color', null)
-    d3.select(this).style('color', '#E5ABA9')
-  })
+        d3.transition().duration(750).each(function () {
+
+            var bars = surveyChart.selectAll('.surveyRectGroup')
+                .data(filtered);
+
+            bars.exit().transition()
+                .style('opacity', '1e-6').remove();
+
+            bars.enter().append('g')
+                .attr('class', 'surveyRectGroup');
+
+            var barRect = bars.selectAll('rect')
+                .data(function (d) { return d; })
+            .enter().append('rect')
+                .attr('width', x.rangeBand())
+                .attr('x', function (d) { return x(d.x); })
+                .attr('y', function (d) { return y(d.y0 + d.y); })
+                .attr('height', 0);
+
+            var barText = bars.selectAll('.barText')
+                .data(function (d) { return d; })
+            .enter().append('text')
+                .attr('class', 'barText')
+                .style('text-anchor', 'middle')
+                .style('visibility', 'hidden')
+                .text(function (d) { return formatPercent(d.y); });
+
+            var legend = bars.selectAll('.legend')
+                .data(function (d) {
+                    return d.filter(function (c) {
+                        return c.x === 'USA';
+                    });
+                });
+
+            legend.enter().append('text')
+                .attr('class', 'legend')
+                .attr('transform', function (d) {
+                    return 'translate(' + (x(d.x) + x.rangeBand()) + ',' + (y(d.y0 + d.y) + (y(d.y0) - y(d.y0 + d.y)) / 2) + ')';
+                })
+                .attr('x', 5)
+                .attr('dy', '.35em')
+                .text(function (d) { return d.name; });
+
+            // transitions
+            bars.selectAll('rect').transition()
+                .attr('y', function (d) { return y(d.y0 + d.y); })
+                .attr('height', function (d) { return y(d.y0) - y(d.y0 + d.y); })
+                .style('fill', function (d) {
+                    d.region === 'Asia' ?
+                    color.range(colorBlends.red)
+                    : color.range(colorBlends.blue);
+                    return color(d.name);
+                })
+                .each('end', function () {
+                    d3.select(this).attr('class', function (d) {
+                        return cleanClass(d.name);
+                    });
+                });
+
+            bars.selectAll('.barText').transition()
+                .attr('y', function (d) { return y(d.y0 + d.y) + (y(d.y0) - y(d.y0 + d.y)) / 2; })
+                .attr('x', function (d) { return x(d.x) + x.rangeBand() / 2; })
+                .attr('dy', function (d) { return (d.y) < 0.025 ? '.5em' : '.35em'; })
+                .style('opacity', function (d) { return (d.y) === 0 ? 1e-6 : 1; })
+                .text(function (d) { return formatPercent(d.y); })
+                .each('end', function () {
+                    d3.select(this).attr('class', function (d) { return cleanClass(d.name) + ' barText' ; });
+                });
+
+            bars.selectAll('.legend').transition()
+                .attr('transform', function (d) { return 'translate(' + (x(d.x) + x.rangeBand()) + ',' + (y(d.y0 + d.y) + (y(d.y0) - y(d.y0 + d.y)) / 2) + ')'; })
+                .text(function (d) { return d.name; });
+
+            d3.selectAll('.surveyRectGroup rect').on('mouseover', mouseOn);
+            d3.selectAll('.surveyRectGroup rect').on('mouseout', mouseOff);
+            d3.selectAll('.barText').on('mouseover', mouseOn);
+            d3.selectAll('.barText').on('mouseout', mouseOff);
+        });
+    };
+
+    function cleanClass(name) {
+        return name.replace(/\s/g, '').replace('/', '');
+    }
+
+    function mouseOn() {
+        var thisClass = d3.select(this).attr('class');
+        d3.selectAll('text.' + thisClass).style('visibility', 'visible');
+        d3.select(this).style('visibility', 'visible');
+    }
+
+    function mouseOff() {
+        var thisClass = d3.select(this).attr('class');
+        d3.selectAll('text.' + thisClass).style('visibility', 'hidden');
+    }
+
+    d3.selectAll('.surveyControl').on('click', function () {
+        var updateVal = d3.select(this).property('id');
+        setData(updateVal);
+
+        d3.select('#questText').text(questions[updateVal]);
+
+        d3.selectAll('.surveyControl').style('color', null);
+        d3.select(this).style('color', '#E5ABA9');
+    })
 })
